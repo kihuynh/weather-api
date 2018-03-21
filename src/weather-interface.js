4f47b4fd2d205d4afa8377cebd14bbb9b54cc39e
Guest User <Guest@Epicodus-5D.local>
2018-03-21 10:41:03 -0700

    made into es6 promises

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,17 +1,24 @@
 export function Weather(city){
-  // alert(city);
-  $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=30598242710b31a13b186fec41d7df3c`,
-    type: 'GET',
-    data: {
-      format: 'json'
-    },
-    success: function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-    },
-    error: function() {
-      $('#errors').text("There was an error processing your request. Please try again.")
-    }
-  });
+  let promise = new Promise(function(resolve, reject) {
+     let request = new XMLHttpRequest();
+     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=30598242710b31a13b186fec41d7df3c
+`;
+     request.onload = function() {
+       if (this.status === 200) {
+         resolve(request.response);
+       } else {
+         reject(Error(request.statusText));
+       }
+     }
+     request.open("GET", url, true);
+     request.send();
+   });
+
+   promise.then(function(response) {
+     let body = JSON.parse(response);
+     $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+     $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+   }, function(error) {
+     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+   });
 }
